3c5da66db898f757ac2cdf5ad739c368c93d2534
Hespian <kamil.gurlaga1999@gmail.com>
2025-09-17 21:33:50 +0200

    Ordinary Least Squares and Ridge Regression

--- Linear_models.py
+++ Linear_models.py
@@ -1,11 +1,61 @@
-# 1.1.1. Ordinary Least Squeres
+# 1.1.1. Ordinary Least Squeressdasdsdsadasdsadasd
 from sklearn import linear_model
 import numpy as np
 reg = linear_model.LinearRegression()
 reg.fit([[0, 0], [1, 1], [2, 2]], [0, 1, 2])
 print(reg.coef_)
+
+
 intercept = reg.intercept_
 intercept = 0 if np.isclose(intercept, 0) else intercept
 print(intercept)
 
 # Ordinary Least Squares and Ridge Regression
+# Authors: The scikit-learn developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+# Data Loading and Preparation
+from sklearn.datasets import load_diabetes
+from sklearn.model_selection import train_test_split
+
+X, y = load_diabetes(return_X_y=True)
+X = X[:, [2]]  # Use only one feature
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=20, shuffle=False)
+
+# Linear regression model
+from sklearn.linear_model import LinearRegression
+
+regressor = LinearRegression().fit(X_train, y_train)
+
+# Model evaluation
+from sklearn.metrics import mean_squared_error, r2_score
+
+y_pred = regressor.predict(X_test)
+
+print(f"Mean squared error: {mean_squared_error(y_test, y_pred):.2f}")
+print(f"Coefficient of determination: {r2_score(y_test, y_pred):.2f}")
+
+# Plotting the results
+import matplotlib.pyplot as plt
+
+fig, ax = plt.subplots(ncols=2, figsize=(10, 5), sharex=True, sharey=True)
+
+ax[0].scatter(X_train, y_train, label="Train data points")
+ax[0].plot(
+    X_train,
+    regressor.predict(X_train),
+    linewidth=3,
+    color="tab:orange",
+    label="Model predictions",
+)
+ax[0].set(xlabel="Feature", ylabel="Target", title="Train set")
+ax[0].legend()
+
+ax[1].scatter(X_test, y_test, label="Test data points")
+ax[1].plot(X_test, y_pred, linewidth=3, color="tab:orange", label="Model predictions")
+ax[1].set(xlabel="Feature", ylabel="Target", title="Test set")
+ax[1].legend()
+
+fig.suptitle("Linear Regression")
+
+plt.show()
