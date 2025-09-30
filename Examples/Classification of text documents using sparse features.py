9bf6797eadf1463f826ff19b78477107a16fe51a
Hespian <kamil.gurlaga1999@gmail.com>
2025-09-30 20:10:01 +0200

    Examples/Classification of text documents using sparse features #2

--- Examples/Classification of text documents using sparse features.py
+++ Examples/Classification of text documents using sparse features.py
@@ -160,3 +160,121 @@ for doc in data_train.data:
     if "caltech" in doc:
         print(doc)
         break
+
+## Model with metadata stripping
+(
+    X_train,
+    X_test,
+    y_train,
+    y_test,
+    feature_names,
+    target_names,
+) = load_dataset(remove = ("headers", "footers", "quotes"))
+
+clf = RidgeClassifier(tol = 1e-2, solver = "sparse_cg")
+clf.fit(X_train, y_train)
+pred = clf.predict(X_test)
+
+fig, ax = plt.subplots(figsize = (10, 5))
+ConfusionMatrixDisplay.from_predictions(y_test, pred, ax = ax)
+ax.xaxis.set_ticklabels(target_names)
+ax.yaxis.set_ticklabels(target_names)
+_ = ax.set_title(f"Confusion Matrix for {clf.__class__.__name__}\non filtered documents")
+plt.show()
+
+_ = plot_feature_effects().set_title("Average feature effects on filtered documents")
+plt.show()
+
+## Benchmarking classifiers
+from sklearn import metrics
+from sklearn.utils.extmath import density
+
+def benchmark(clf, custom_name = False):
+    print("_" * 80)
+    print("Training: ")
+    print(clf)
+    t0 = time()
+    clf.fit(X_train, y_train)
+    train_time = time() - t0
+    print(f"train time: {train_time:.3}s")
+
+    t0 = time()
+    pred = clf.predict(X_test)
+    test_time = time() - t0
+    print(f"test time: {test_time:.3}s")
+
+    score = metrics.accuracy_score(y_test, pred)
+    print(f"accuracy: {score:.3}")
+
+    if hasattr(clf, "coef_"):
+        print(f"dimensionality: {clf.coef_.shape[1]}")
+        print(f"density: {density(clf.coef_)}")
+        print()
+    
+    print()
+    if custom_name:
+        clf_descr = str(custom_name)
+    else:
+        clf_descr = clf.__class__.__name__
+    return clf_descr, score, train_time, test_time
+
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.linear_model import LogisticRegression, SGDClassifier
+from sklearn.naive_bayes import ComplementNB
+from sklearn.neighbors import KNeighborsClassifier, NearestCentroid
+from sklearn.svm import LinearSVC
+
+results = []
+for clf, name in (
+    (LogisticRegression(C = 5, max_iter = 1000), "Logistic Regression"),
+    (RidgeClassifier(alpha = 1.0, solver = "sparse_cg"), "Ridge Classifier"),
+    (KNeighborsClassifier(n_neighbors = 100), "kNN"),
+    (RandomForestClassifier(), "Random Forest"),
+    # L2 penalty Linear SVC
+    (LinearSVC(C = 0.1, dual = False, max_iter = 1000), "Linear SVC"),
+    # L2 penalty LInear SGD
+    (
+        SGDClassifier(
+            loss = "log_loss", alpha = 1e-4, n_iter_no_change = 3, early_stopping = True
+        ),
+        "log-loss SGD",
+    ),
+    # NearestCentroid (aka Rocchio classifier)
+    (NearestCentroid(), "NearestCentroid"),
+    # Sparse naive Bayes classifier
+    (ComplementNB(alpha = 0.1), "Complement naive Bayes"),
+):
+    print("=" * 80)
+    print(name)
+    results.append(benchmark(clf, name))
+
+## Plot accuracy, training and test time of each classifier
+indices = np.arange(len(results))
+
+results = [[x[i] for x in results] for i in range(4)]
+
+clf_names, score, training_time, test_time = results
+training_time = np.array(training_time)
+test_time = np.array(test_time)
+
+fig, ax1 = plt.subplots(figsize = (10, 8))
+ax1.scatter(score, training_time, s = 60)
+ax1.set(
+    title = "Score-training time trade-off",
+    yscale = "log",
+    xlabel = "test accuracy",
+    ylabel = "training time (s)",
+)
+fig, ax2 = plt.subplots(figsize = (10, 8))
+ax2.scatter(score, test_time, s = 60)
+ax2.set(
+    title = "Score-test time trade-off",
+    yscale = "log",
+    xlabel = "test accuracy",
+    ylabel = "test time (s)",
+)
+
+for i, txt in enumerate(clf_names):
+    ax1.annotate(txt, (score[i], training_time[i]))
+    ax2.annotate(txt, (score[i], test_time[i]))
+plt.show()
