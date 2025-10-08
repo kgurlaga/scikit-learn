83aa5b16b2c8767c471f69ded65893ec119aeb12
Hespian <kamil.gurlaga1999@gmail.com>
2025-10-08 21:32:09 +0200

    Lasso model selection: AIC-BIC / cross-validation #2

--- Examples/Lasso model selection AIC-BIC cross-validation.py
+++ Examples/Lasso model selection AIC-BIC cross-validation.py
@@ -2,6 +2,7 @@
 # SPDX-License-Identifier: BSD-3-Clause
 
 ##DATASET
+import matplotlib.pyplot as plt
 from sklearn.datasets import load_diabetes
 X, y = load_diabetes(return_X_y = True, as_frame = True)
 X.head()
@@ -24,3 +25,109 @@ X = pd.concat([X, X_random], axis = 1)
 X[X.columns[::3]].head()
 
 ## Selecting Lasso via an information criterion
+import time
+from sklearn.linear_model import LassoLarsIC
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+start_time = time.time()
+lasso_lars_ic = make_pipeline(StandardScaler(), LassoLarsIC(criterion = "aic")).fit(X, y)
+fit_time = time.time() - start_time
+
+results = pd.DataFrame(
+    {
+        "alphas": lasso_lars_ic[-1].alphas_,
+        "AIC criterion": lasso_lars_ic[-1].criterion_,
+    }
+).set_index("alphas")
+alpha_aic = lasso_lars_ic[-1].alpha_
+
+lasso_lars_ic.set_params(lassolarsic__criterion = "bic").fit(X, y)
+results["BIC criterion"] = lasso_lars_ic[-1].criterion_
+alpha_bic = lasso_lars_ic[-1].alpha_
+
+def highlight_min(x):
+    x_min = x.min()
+    return ["font-weight: bold" if v == x_min else "" for v in x]
+
+
+results.style.apply(highlight_min)
+
+#Plot AIC and BIC
+ax = results.plot()
+ax.vlines(
+    alpha_aic,
+    results["AIC criterion"].min(),
+    results["AIC criterion"].max(),
+    label = "alpha: AIC estimate",
+    linestyles = "--",
+    color = "tab:blue",
+)
+ax.vlines(
+    alpha_bic,
+    results["BIC criterion"].min(),
+    results["BIC criterion"].max(),
+    label = "alpha: BIC estimate",
+    linestyle = "--",
+    color = "tab:orange",
+)
+ax.set_xlabel(r"$\alpha$")
+ax.set_ylabel("criterion")
+ax.set_xscale("log")
+ax.legend()
+_ = ax.set_title(
+    f"Information-criterion for model selection (training time {fit_time:.2f}s)"
+)
+plt.show()
+
+##SELECTING LASSO VIA CROSS-VALIDATION
+#Lasso via coordinate descent
+from sklearn.linear_model import LassoCV
+
+start_time = time.time()
+model = make_pipeline(StandardScaler(), LassoCV(cv = 20)).fit(X, y)
+fit_time = time.time() - start_time
+
+ymin, ymax = 2300, 3800
+lasso = model[-1]
+plt.semilogx(lasso.alphas_, lasso.mse_path_, linestyle = ":")
+plt.plot(
+    lasso.alphas_,
+    lasso.mse_path_.mean(axis = -1),
+    color = "black",
+    label = "Average across the folds",
+    linewidth = 2,
+)
+plt.axvline(lasso.alpha_, linestyle = "--", color = "black", label = "alpha: CV estimate")
+plt.ylim(ymin, ymax)
+plt.xlabel(r"$\alpha$")
+plt.ylabel("Mean square error")
+plt.legend()
+_  = plt.title(
+    f"Mean square error on each fold: coordinate descent (train time: {fit_time:.2f}s)"
+)
+plt.show()
+
+#Lasso via least angle regression
+from sklearn.linear_model import LassoLarsCV
+
+start_time = time.time()
+model = make_pipeline(StandardScaler(), LassoLarsCV(cv = 20)).fit(X, y)
+fit_time = time.time() - start_time
+
+lasso = model[-1]
+plt.semilogx(lasso.cv_alphas_, lasso.mse_path_, ":")
+plt.semilogx(
+    lasso.cv_alphas_,
+    lasso.mse_path_.mean(axis = -1),
+    color = "black",
+    label = "Average across the folds",
+    linewidth = 2,
+)
+plt.axvline(lasso.alpha_, linestyle = "--", color = "black", label = "alpha CV")
+plt.ylim(ymin, ymax)
+plt.xlabel(r"$\alpha$")
+plt.ylabel("Mean square error")
+plt.legend()
+_ = plt.title(f"Mean square error on each fold: Lars (train time: {fit_time:.2f}s)")
+plt.show()
