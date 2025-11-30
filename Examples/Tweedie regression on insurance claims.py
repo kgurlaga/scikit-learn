7b83dffe87a0c38d3cf8c52763a51b912219e0a9
Hespian <kamil.gurlaga1999@gmail.com>
2025-11-30 12:29:50 +0100

    Tweedie regression on insurance claims

---
+++ Examples/Tweedie regression on insurance claims.py
@@ -0,0 +1,157 @@
+from functools import partial
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+from sklearn.datasets import fetch_openml
+from sklearn.metrics import (
+    mean_absolute_error,
+    mean_squared_error,
+    mean_tweedie_deviance,
+)
+
+
+def load_mtpl2(n_samples=None):
+    """Fetch the French Motor Third-Party Liability Claims dataset.
+
+    Parameters
+    ----------
+    n_samples: int, default=None
+      number of samples to select (for faster run time). Full dataset has
+      678013 samples.
+    """
+    # freMTPL2freq dataset from https://www.openml.org/d/41214
+    df_freq = fetch_openml(data_id=41214, as_frame=True).data
+    df_freq["IDpol"] = df_freq["IDpol"].astype(int)
+    df_freq.set_index("IDpol", inplace=True)
+
+    # freMTPL2sev dataset from https://www.openml.org/d/41215
+    df_sev = fetch_openml(data_id=41215, as_frame=True).data
+
+    # sum ClaimAmount over identical IDs
+    df_sev = df_sev.groupby("IDpol").sum()
+
+    df = df_freq.join(df_sev, how="left")
+    df["ClaimAmount"] = df["ClaimAmount"].fillna(0)
+
+    # unquote string fields
+    for column_name in df.columns[[t is object for t in df.dtypes.values]]:
+        df[column_name] = df[column_name].str.strip("'")
+    return df.iloc[:n_samples]
+
+
+def plot_obs_pred(
+    df,
+    feature,
+    weight,
+    observed,
+    predicted,
+    y_label=None,
+    title=None,
+    ax=None,
+    fill_legend=False,
+):
+    """Plot observed and predicted - aggregated per feature level.
+
+    Parameters
+    ----------
+    df : DataFrame
+        input data
+    feature: str
+        a column name of df for the feature to be plotted
+    weight : str
+        column name of df with the values of weights or exposure
+    observed : str
+        a column name of df with the observed target
+    predicted : DataFrame
+        a dataframe, with the same index as df, with the predicted target
+    fill_legend : bool, default=False
+        whether to show fill_between legend
+    """
+    # aggregate observed and predicted variables by feature level
+    df_ = df.loc[:, [feature, weight]].copy()
+    df_["observed"] = df[observed] * df[weight]
+    df_["predicted"] = predicted * df[weight]
+    df_ = (
+        df_.groupby([feature])[[weight, "observed", "predicted"]]
+        .sum()
+        .assign(observed=lambda x: x["observed"] / x[weight])
+        .assign(predicted=lambda x: x["predicted"] / x[weight])
+    )
+
+    ax = df_.loc[:, ["observed", "predicted"]].plot(style=".", ax=ax)
+    y_max = df_.loc[:, ["observed", "predicted"]].values.max() * 0.8
+    p2 = ax.fill_between(
+        df_.index,
+        0,
+        y_max * df_[weight] / df_[weight].values.max(),
+        color="g",
+        alpha=0.1,
+    )
+    if fill_legend:
+        ax.legend([p2], ["{} distribution".format(feature)])
+    ax.set(
+        ylabel=y_label if y_label is not None else None,
+        title=title if title is not None else "Train: Observed vs Predicted",
+    )
+
+
+def score_estimator(
+    estimator,
+    X_train,
+    X_test,
+    df_train,
+    df_test,
+    target,
+    weights,
+    tweedie_powers=None,
+):
+    """Evaluate an estimator on train and test sets with different metrics"""
+
+    metrics = [
+        ("D² explained", None),  # Use default scorer if it exists
+        ("mean abs. error", mean_absolute_error),
+        ("mean squared error", mean_squared_error),
+    ]
+    if tweedie_powers:
+        metrics += [
+            (
+                "mean Tweedie dev p={:.4f}".format(power),
+                partial(mean_tweedie_deviance, power=power),
+            )
+            for power in tweedie_powers
+        ]
+
+    res = []
+    for subset_label, X, df in [
+        ("train", X_train, df_train),
+        ("test", X_test, df_test),
+    ]:
+        y, _weights = df[target], df[weights]
+        for score_label, metric in metrics:
+            if isinstance(estimator, tuple) and len(estimator) == 2:
+                # Score the model consisting of the product of frequency and
+                # severity models.
+                est_freq, est_sev = estimator
+                y_pred = est_freq.predict(X) * est_sev.predict(X)
+            else:
+                y_pred = estimator.predict(X)
+
+            if metric is None:
+                if not hasattr(estimator, "score"):
+                    continue
+                score = estimator.score(X, y, sample_weight=_weights)
+            else:
+                score = metric(y, y_pred, sample_weight=_weights)
+
+            res.append({"subset": subset_label, "metric": score_label, "score": score})
+
+    res = (
+        pd.DataFrame(res)
+        .set_index(["metric", "subset"])
+        .score.unstack(-1)
+        .round(4)
+        .loc[:, ["train", "test"]]
+    )
+    return res
