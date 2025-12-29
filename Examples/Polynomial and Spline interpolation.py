cf243918ea0375c93e579c0936e4f8c7a2c9d8f4
Hespian <kamil.gurlaga1999@gmail.com>
2025-12-29 18:22:22 +0100

    Periodic Splines

--- Examples/Polynomial and Spline interpolation.py
+++ Examples/Polynomial and Spline interpolation.py
@@ -51,3 +51,66 @@ ax.plot(x_plot, y_plot, label="B-Spline")
 ax.legend(loc="lower center")
 ax.set_ylim(-20, 10)
 plt.show()
+
+####################################################
+fig, axes = plt.subplots(ncols=2, figsize=(16, 5))
+pft = PolynomialFeatures(degree=3).fit(X_train)
+axes[0].plot(x_plot, pft.transform(X_plot))
+axes[0].legend(axes[0].lines, [f"degree {n}" for n in range(4)])
+axes[0].set_title("PolynomialFeatures")
+
+splt = SplineTransformer(n_knots=4, degree=3).fit(X_train)
+axes[1].plot(x_plot, splt.transform(X_plot))
+axes[1].legend(axes[1].lines, [f"spline {n}" for n in range(6)])
+axes[1].set_title("SplineTransformer")
+
+# plot knots of spline
+knots = splt.bsplines_[0].t
+axes[1].vlines(knots[3:-3], ymin=0, ymax=0.8, linestyles="dashed")
+plt.show()
+
+####################################################
+# Periodic Splines
+def g(x):
+    """Function to be approximated by periodic spline interpolation."""
+    return np.sin(x) - 0.7 * np.cos(x * 3)
+
+y_train = g(x_train)
+
+# Extend the test data into the future:
+x_plot_ext = np.linspace(-1, 21, 200)
+X_plot_ext = x_plot_ext[:, np.newaxis]
+
+lw = 2
+fig, ax = plt.subplots()
+ax.set_prop_cycle(color=["black", "tomato", "teal"])
+ax.plot(x_plot_ext, g(x_plot_ext), linewidth=lw, label="ground truth")
+ax.scatter(x_train, y_train, label="training points")
+
+for transformer, label in [
+    (SplineTransformer(degree=3, n_knots=10), "spline"),
+    (
+        SplineTransformer(
+            degree=3,
+            knots=np.linspace(0, 2 * np.pi, 10)[:, None],
+            extrapolation="periodic",
+        ),
+        "periodic spline",
+    ),
+]:
+    model = make_pipeline(transformer, Ridge(alpha=1e-3))
+    model.fit(X_train, y_train)
+    y_plot_ext = model.predict(X_plot_ext)
+    ax.plot(x_plot_ext, y_plot_ext, label=label)
+
+ax.legend()
+fig.show()
+plt.show()
+
+####################################################
+fig, ax = plt.subplots()
+knots = np.linspace(0, 2 * np.pi, 4)
+splt = SplineTransformer(knots=knots[:, None], degree=3, extrapolation="periodic").fit(X_train)
+ax.plot(x_plot_ext, splt.transform(X_plot_ext))
+ax.legend(ax.lines, [f"spline {n}" for n in range(3)])
+plt.show()
