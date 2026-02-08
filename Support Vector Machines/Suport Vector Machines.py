45cabebb3e428df5a0c98547d9882a49f59f92fc
Hespian <kamil.gurlaga1999@gmail.com>
2026-02-08 14:35:20 +0100

    Kernel functions

--- Support Vector Machines/Suport Vector Machines.py
+++ Support Vector Machines/Suport Vector Machines.py
@@ -43,3 +43,11 @@ y = [0.5, 2.5]
 regr = svm.SVR()
 regr.fit(X, y)
 regr.predict([[1, 1]])
+
+
+####################################################
+## KERNEL FUNCTIONS
+linear_svc = svm.SVC(kernel='linear')
+linear_svc.kernel
+rbf_svc = svm.SVC(kernel='rbf')
+rbf_svc.kernel
