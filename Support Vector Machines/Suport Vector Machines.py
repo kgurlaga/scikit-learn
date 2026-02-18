9068764071fc8e6f63b65ab560d8d27f9f096775
Hespian <kamil.gurlaga1999@gmail.com>
2026-02-18 16:27:19 +0100

    SVM with custom kernel

--- Support Vector Machines/Suport Vector Machines.py
+++ Support Vector Machines/Suport Vector Machines.py
@@ -51,3 +51,24 @@ linear_svc = svm.SVC(kernel='linear')
 linear_svc.kernel
 rbf_svc = svm.SVC(kernel='rbf')
 rbf_svc.kernel
+
+####################################################
+## CUSTOM KERNELS
+import numpy as np
+from sklearn import svm
+def my_kernel(X, Y):
+    return np.dot(X, Y.T)
+clf = svm.SVC(kernel=my_kernel)
+
+# Using the Gram matrix
+import numpy as np
+from sklearn.datasets import make_classification
+from sklearn.model_selection import train_test_split
+from sklearn import svm
+X, y = make_classification(n_samples=10, random_state=0)
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=0)
+clf = svm.SVC(kernel='precomputed')
+gram_train = np.dot(X_train, X_train.T)
+clf.fit(gram_train, y_train)
+gram_test = np.dot(X_test, X_train.T)
+clf.predict(gram_test)
