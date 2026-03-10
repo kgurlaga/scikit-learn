f8280d0dfe9a4125137b4b62d69c9089f4571762
Hespian <kamil.gurlaga1999@gmail.com>
2026-03-10 20:03:56 +0100

    Prediction latency 2

--- Stochastic Gradient Descent/prediction_latency.py
+++ Stochastic Gradient Descent/prediction_latency.py
@@ -100,3 +100,152 @@ def generate_dataset(n_train, n_test, n_features, noise=0.1, verbose=False):
     if verbose:
         print("ok")
     return X_train, y_train, X_test, y_test
+
+def boxplot_runtimes(runtimes, pred_type, configuration):
+    """
+    Plot a new `Figure` with boxplots of prediction runtimes.
+
+    Parameters
+    ----------
+    runtime : list of `np.array` of latencies in micro-seconds
+    cls_names : list of estimator class names that generated the runtimes
+    pred_type : 'bulk' or 'atomic'
+    """
+
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+    bp = plt.boxplot(
+        runtimes,
+    )
+
+    cls_infos = [
+        "%s\n(%d %s)"
+        % (
+            estimator_conf["name"],
+            estimator_conf["complexity_computer"](estimator_conf["instance"]),
+            estimator_conf["complexity_label"],
+        )
+        for estimator_conf in configuration["estimators"]
+    ]
+    plt.setp(ax1, xticklabels=cls_infos)
+    plt.setp(bp["boxes"], color="black")
+    plt.setp(bp["whiskers"], color="black")
+    plt.setp(bp["fliers"], color="red", marker="+")
+
+    ax1.yaxis.grid(True, linestyle="-", which="major", color="lightgrey", alpha=0.5)
+    ax1.set_axisbelow(True)
+    ax1.set_title(
+        "Prediction Time per Instance - %s, %d feats."
+        % (pred_type.capitalize(), configuration["n_features"])
+    )
+    ax1.set_ylabel("Prediction Time (us)")
+    plt.show()
+
+def benchmark(configuration):
+    """Run the whole benchmark."""
+    X_train, y_train, X_test, y_test = generate_dataset(
+        configuration["n_train"], configuration["n_test"], configuration["n_features"]
+    )
+    stats = {}
+    for estimator_conf in configuration["estimators"]:
+        print("Benchmarking", estimator_conf["instance"])
+        estimator_conf["instance"].fit(X_train, y_train)
+        gc.collect()
+        a, b = benchmark_estimator(estimator_conf["instance"], X_test)
+        stats[estimator_conf["name"]] = {"atomic": a, "bulk": b}
+    
+    cls_names = [
+        estimator_conf["name"] for estimator_conf in configuration["estimators"]
+    ]
+    runtimes = [1e6 * stats[clf_name]["atomic"] for clf_name in cls_names]
+    boxplot_runtimes(runtimes, "atomic", configuration)
+    runtimes = [1e6 * stats[clf_name]["bulk"] for clf_name in cls_names]
+    boxplot_runtimes(runtimes, "bulk (%d)" % configuration["n_test"], configuration)
+
+def n_feature_influence(estimators, n_train, n_test, n_features, percentile):
+    """
+    Estimate influence of the number of features on prediction time.
+
+    Parameters
+    ----------
+    estimators : dict of (name(str), estimator) to benchmark
+    n_train : nber of training instances (int)
+    n_test : nber of testing instances (int)
+    n_features : list of feature-space dimensionality to test (int)
+    percentile : percentile at which to measure the speed (int[0-100])
+
+    Returns:
+    -------
+    percentiles : dict(estimator_name, dict(n_features, percentile_perf_in_us))
+    """
+    percentiles = defaultdict(defaultdict)
+    for n in n_features:
+        print("benchmarking with %d features" %n)
+        X_train, y_train, X_test, y_test = generate_dataset(n_train, n_test, n)
+        for cls_name, estimator in estimators.items():
+            estimator.fit(X_train, y_train)
+            gc.collect()
+            runtimes = bulk_benchmark_estimator(estimator, X_test, 30, False)
+            percentiles[cls_name][n] = 1e6 * np.percentile(runtimes, percentile)
+    return percentiles
+
+def plot_n_features_influence(percentiles, percentile):
+    fig, ax1 = plt.subplots(figsize=(10, 6))
+    colors = ["r", "g", "b"]
+    for i, cls_name in enumerate(percentiles.keys()):
+        x = np.array(sorted(percentiles[cls_name].keys()))
+        y = np.array([percentiles[cls_name][n] for n in x])
+        plt.plot(
+            x,
+            y,
+            color=colors[i],
+        )
+    ax1.yaxis.grid(True, linestyle="-", which="major", color="lightgrey", alpha=0.5)
+    ax1.set_axisbelow(True)
+    ax1.set_title("Evolution of Prediction Time with #Features")
+    ax1.set_xlabel("#Features")
+    ax1.set_ylabel("Prediction Time at %d%%-ile (us)" % percentile)
+    plt.show()
+
+def benchmark_throughputs(configuration, duration_secs=0.1):
+    """benchmark throughput for different estimators."""
+    X_train, y_train, X_test, y_test = generate_dataset(
+        configuration["n_train"], configuration["n_test"], configuration["n_features"]
+    )
+    throughputs = dict()
+    for estimator_config in configuration["estimators"]:
+        estimator_config["instance"].fit(X_train, y_train)
+        start_time =time.time()
+        n_predictions = 0
+        while (time.time() - start_time) < duration_secs:
+            estimator_config["instance"].predict(X_test[[0]])
+            n_predictions += 1
+        throughputs[estimator_config["name"]] = n_predictions / duration_secs
+    return throughputs
+
+def plot_benchmark_throughput(throughputs, configuration):
+    fig, ax = plt.subplots(figsize=(10, 6))
+    colors = ["r", "g", "b"]
+    cls_infos = [
+        "%s\n(%d %s)"
+        % (
+            estimator_conf["name"],
+            estimator_conf["complexity_computer"](estimator_conf["instance"]),
+            estimator_conf["complexity_label"],
+        )
+        for estimator_conf in configuration["estimators"]
+    ]
+    cls_values = [
+        throughputs[estimator_conf["name"]]
+        for estimator_conf in configuration["estimators"]
+    ]
+    plt.bar(range(len(throughputs_)), cls_values, width=0.5, color=colors)
+    ax.set_xticks(np.linspace(0.25, len(throughputs) - 0.75, len(throughputs)))
+    ax.set_xticklabels(cls_infos, fontsize=10)
+    ymax = max(cls_values) * 1.2
+    ax.set_ylim((0, ymax))
+    ax.set_ylabel("Throughput (predictions/sec)")
+    ax.set_title(
+        "Prediction Throughput for different estimators (%d features)"
+        % configuration["n_features"]
+    )
+    plt.show()
