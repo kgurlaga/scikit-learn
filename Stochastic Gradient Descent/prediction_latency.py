d2c4db66f5d3f68096b4b63116dc7414f987e7ca
Hespian <kamil.gurlaga1999@gmail.com>
2026-03-15 21:25:23 +0100

    Prediction latency 3

--- Stochastic Gradient Descent/prediction_latency.py
+++ Stochastic Gradient Descent/prediction_latency.py
@@ -1,10 +1,13 @@
 # Authors: The scikit-learn developers
 # SPDX-License-Identifier: BSD-3-Clause
+
 import gc
 import time
 from collections import defaultdict
+
 import matplotlib.pyplot as plt
 import numpy as np
+
 from sklearn.datasets import make_regression
 from sklearn.ensemble import RandomForestRegressor
 from sklearn.linear_model import Ridge, SGDRegressor
@@ -13,11 +16,11 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.svm import SVR
 from sklearn.utils import shuffle
 
+
 def _not_in_sphinx():
     # Hack to detect whether we are running by the sphinx builder
     return "__file__" in globals()
 
-# BENCHMARK AND PLOT HELPER FUNCTIONS
 def atomic_benchmark_estimator(estimator, X_test, verbose=False):
     """Measure runtime prediction of each instance."""
     n_instances = X_test.shape[0]
@@ -35,6 +38,7 @@ def atomic_benchmark_estimator(estimator, X_test, verbose=False):
             max(runtimes),
         )
     return runtimes
+
 
 def bulk_benchmark_estimator(estimator, X_test, n_bulk_repeats, verbose):
     """Measure runtime prediction of the whole input."""
@@ -54,24 +58,27 @@ def bulk_benchmark_estimator(estimator, X_test, n_bulk_repeats, verbose):
         )
     return runtimes
 
+
 def benchmark_estimator(estimator, X_test, n_bulk_repeats=30, verbose=False):
     """
     Measure runtimes of prediction in both atomic and bulk mode.
 
     Parameters
     ----------
-    estimator: already trained estimator supporting `predict()`
-    X_test: test input
-    n_bulk_repeats: how many times to repeat when evaluating bulk mode
+    estimator : already trained estimator supporting `predict()`
+    X_test : test input
+    n_bulk_repeats : how many times to repeat when evaluating bulk mode
 
     Returns
     -------
-    atomic_runtimes, bulk_runtimes: a pair of `np.array` which contain the runtimes in seconds.
+    atomic_runtimes, bulk_runtimes : a pair of `np.array` which contain the
+    runtimes in seconds.
 
     """
     atomic_runtimes = atomic_benchmark_estimator(estimator, X_test, verbose)
     bulk_runtimes = bulk_benchmark_estimator(estimator, X_test, n_bulk_repeats, verbose)
     return atomic_runtimes, bulk_runtimes
+
 
 def generate_dataset(n_train, n_test, n_features, noise=0.1, verbose=False):
     """Generate a regression dataset with the given parameters."""
@@ -101,15 +108,17 @@ def generate_dataset(n_train, n_test, n_features, noise=0.1, verbose=False):
         print("ok")
     return X_train, y_train, X_test, y_test
 
+
 def boxplot_runtimes(runtimes, pred_type, configuration):
     """
     Plot a new `Figure` with boxplots of prediction runtimes.
 
     Parameters
     ----------
-    runtime : list of `np.array` of latencies in micro-seconds
+    runtimes : list of `np.array` of latencies in micro-seconds
     cls_names : list of estimator class names that generated the runtimes
     pred_type : 'bulk' or 'atomic'
+
     """
 
     fig, ax1 = plt.subplots(figsize=(10, 6))
@@ -132,19 +141,23 @@ def boxplot_runtimes(runtimes, pred_type, configuration):
     plt.setp(bp["fliers"], color="red", marker="+")
 
     ax1.yaxis.grid(True, linestyle="-", which="major", color="lightgrey", alpha=0.5)
+
     ax1.set_axisbelow(True)
     ax1.set_title(
         "Prediction Time per Instance - %s, %d feats."
         % (pred_type.capitalize(), configuration["n_features"])
     )
     ax1.set_ylabel("Prediction Time (us)")
+
     plt.show()
+
 
 def benchmark(configuration):
     """Run the whole benchmark."""
     X_train, y_train, X_test, y_test = generate_dataset(
         configuration["n_train"], configuration["n_test"], configuration["n_features"]
     )
+
     stats = {}
     for estimator_conf in configuration["estimators"]:
         print("Benchmarking", estimator_conf["instance"])
@@ -152,7 +165,7 @@ def benchmark(configuration):
         gc.collect()
         a, b = benchmark_estimator(estimator_conf["instance"], X_test)
         stats[estimator_conf["name"]] = {"atomic": a, "bulk": b}
-    
+
     cls_names = [
         estimator_conf["name"] for estimator_conf in configuration["estimators"]
     ]
@@ -161,25 +174,30 @@ def benchmark(configuration):
     runtimes = [1e6 * stats[clf_name]["bulk"] for clf_name in cls_names]
     boxplot_runtimes(runtimes, "bulk (%d)" % configuration["n_test"], configuration)
 
+
 def n_feature_influence(estimators, n_train, n_test, n_features, percentile):
     """
     Estimate influence of the number of features on prediction time.
 
     Parameters
     ----------
-    estimators : dict of (name(str), estimator) to benchmark
+
+    estimators : dict of (name (str), estimator) to benchmark
     n_train : nber of training instances (int)
     n_test : nber of testing instances (int)
     n_features : list of feature-space dimensionality to test (int)
-    percentile : percentile at which to measure the speed (int[0-100])
+    percentile : percentile at which to measure the speed (int [0-100])
 
     Returns:
-    -------
-    percentiles : dict(estimator_name, dict(n_features, percentile_perf_in_us))
+    --------
+
+    percentiles : dict(estimator_name,
+                       dict(n_features, percentile_perf_in_us))
+
     """
     percentiles = defaultdict(defaultdict)
     for n in n_features:
-        print("benchmarking with %d features" %n)
+        print("benchmarking with %d features" % n)
         X_train, y_train, X_test, y_test = generate_dataset(n_train, n_test, n)
         for cls_name, estimator in estimators.items():
             estimator.fit(X_train, y_train)
@@ -187,6 +205,7 @@ def n_feature_influence(estimators, n_train, n_test, n_features, percentile):
             runtimes = bulk_benchmark_estimator(estimator, X_test, 30, False)
             percentiles[cls_name][n] = 1e6 * np.percentile(runtimes, percentile)
     return percentiles
+
 
 def plot_n_features_influence(percentiles, percentile):
     fig, ax1 = plt.subplots(figsize=(10, 6))
@@ -206,6 +225,7 @@ def plot_n_features_influence(percentiles, percentile):
     ax1.set_ylabel("Prediction Time at %d%%-ile (us)" % percentile)
     plt.show()
 
+
 def benchmark_throughputs(configuration, duration_secs=0.1):
     """benchmark throughput for different estimators."""
     X_train, y_train, X_test, y_test = generate_dataset(
@@ -214,13 +234,14 @@ def benchmark_throughputs(configuration, duration_secs=0.1):
     throughputs = dict()
     for estimator_config in configuration["estimators"]:
         estimator_config["instance"].fit(X_train, y_train)
-        start_time =time.time()
+        start_time = time.time()
         n_predictions = 0
         while (time.time() - start_time) < duration_secs:
             estimator_config["instance"].predict(X_test[[0]])
             n_predictions += 1
         throughputs[estimator_config["name"]] = n_predictions / duration_secs
     return throughputs
+
 
 def plot_benchmark_throughput(throughputs, configuration):
     fig, ax = plt.subplots(figsize=(10, 6))
@@ -238,7 +259,7 @@ def plot_benchmark_throughput(throughputs, configuration):
         throughputs[estimator_conf["name"]]
         for estimator_conf in configuration["estimators"]
     ]
-    plt.bar(range(len(throughputs_)), cls_values, width=0.5, color=colors)
+    plt.bar(range(len(throughputs)), cls_values, width=0.5, color=colors)
     ax.set_xticks(np.linspace(0.25, len(throughputs) - 0.75, len(throughputs)))
     ax.set_xticklabels(cls_infos, fontsize=10)
     ymax = max(cls_values) * 1.2
@@ -249,3 +270,32 @@ def plot_benchmark_throughput(throughputs, configuration):
         % configuration["n_features"]
     )
     plt.show()
+
+configuration = {
+    "n_train": int(1e3),
+    "n_test": int(1e2),
+    "n_features": int(1e2),
+    "estimators": [
+        {
+            "name": "Linear Model",
+            "instance": SGDRegressor(
+                penalty="elasticnet", alpha=0.01, l1_ratio=0.25, tol=1e-4
+            ),
+            "complexity_label": "non-zero coefficients",
+            "complexity_computer": lambda clf: np.count_nonzero(clf.coef_),
+        },
+        {
+            "name": "RandomForest",
+            "instance": RandomForestRegressor(),
+            "complexity_label": "estimators",
+            "complexity_computer": lambda clf: clf.n_estimators,
+        },
+        {
+            "name": "SVR",
+            "instance": SVR(kernel="rbf"),
+            "complexity_label": "support vectors",
+            "complexity_computer": lambda clf: len(clf.support_vectors_),
+        },
+    ],
+}
+benchmark(configuration)
