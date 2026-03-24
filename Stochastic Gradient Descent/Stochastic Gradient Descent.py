cf43d972c995969e5b97af9be2f03e25433887d8
Hespian <kamil.gurlaga1999@gmail.com>
2026-03-24 21:26:57 +0100

    SGD

--- Stochastic Gradient Descent/Stochastic Gradient Descent.py
+++ Stochastic Gradient Descent/Stochastic Gradient Descent.py
@@ -18,3 +18,22 @@ clf.predict_proba([[1., 1.]])
 # 1.5.3 Online One-Class SVM
 
 # 1.5.4 Stochastic Gradient Descent for sparse data
+
+# 1.5.5 Complexity
+
+# 1.5.6 Stopping criterion
+
+# 1.5.7 Tips and practical use
+from sklearn.preprocessing import StandardScaler
+scaler = StandardScaler()
+scaler.fit(X_train)
+X_train = scaler.transform(X_train)
+X_test = scaler.transform(X_test) # apply same transformation to test data
+
+# or better yet: use a pipeline!
+from sklearn.pipeline import make_pipeline
+est = make_pipeline(StandardScaler(), SGDClassifier())
+est.fit(X_train)
+est.predict(X_test)
+
+# 1.5.8 Mathematical formulation
