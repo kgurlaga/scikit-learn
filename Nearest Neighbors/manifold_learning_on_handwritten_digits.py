f9b8fad6780c992a14a53fc003801a7402499be1
Hespian <kamil.gurlaga1999@gmail.com>
2026-04-20 21:44:32 +0200

    Manifold learning on handwritten digits: Locally Linear Embedding, Isomap…

---
+++ Nearest Neighbors/manifold_learning_on_handwritten_digits.py
@@ -0,0 +1,19 @@
+# Authors: The scikit-learn developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+## Load digits dataset
+from sklearn.datasets import load_digits
+
+digits = load_digits(n_class=6)
+X, y = digits.data, digits.target
+n_samples, n_features = X.shape
+n_neighbors = 30
+
+import matplotlib.pyplot as plt
+
+fig, axs = plt.subplots(nrows=10, ncols=10, figsize=(6, 6))
+for idx, ax in enumerate(axs.ravel()):
+    ax.imshow(X[idx].reshape((8, 8)), cmap=plt.cm.binary)
+    ax.axis("off")
+_ = fig.suptitle("A selection from the 64-dimensional digits dataset", fontsize=16)
+plt.show()
