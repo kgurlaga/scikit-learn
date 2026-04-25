d51de77638efa12e71b487fc676d1e97f964c696
Hespian <kamil.gurlaga1999@gmail.com>
2026-04-25 16:07:40 +0200

    Ability of Gaussian process regression (GPR) to estimate data noise-level

---
+++ Gaussian Processes/ability_of_gaussian_process_regression_to_estimate_data_noise_level.py
@@ -0,0 +1,142 @@
+# Authors: The scikit-learn developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+## Data generation
+import numpy as np
+
+def target_generator(X, add_noise=False):
+    target = 0.5 + np.sin(3 * X)
+    if add_noise:
+        rng = np.random.RandomState(1)
+        target += rng.normal(0, 0.3, size=target.shape)
+    return target.squeeze()
+
+X = np.linspace(0, 5, num=80).reshape(-1, 1)
+y = target_generator(X, add_noise=False)
+
+import matplotlib.pyplot as plt
+plt.plot(X, y, label="Expected signal")
+plt.legend()
+plt.xlabel("X")
+_ = plt.ylabel("y")
+plt.show()
+
+rng = np.random.RandomState(0)
+X_train = rng.uniform(0, 5, size=20).reshape(-1, 1)
+y_train = target_generator(X_train, add_noise=True)
+
+plt.plot(X, y, label="Expected signal")
+plt.scatter(
+    x=X_train[:, 0],
+    y=y_train,
+    color="black",
+    alpha=0.4,
+    label="Observations",
+)
+plt.legend()
+plt.xlabel("X")
+_ = plt.ylabel("y")
+plt.show()
+
+## Optimisation of kernel hyperparameters in GPR
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF, WhiteKernel
+
+kernel = 1.0 * RBF(length_scale=1e1, length_scale_bounds=(1e-2, 1e3)) + WhiteKernel(
+    noise_level=1, noise_level_bounds=(1e-10, 1e1)
+)
+gpr = GaussianProcessRegressor(kernel=kernel, alpha=0.0)
+gpr.fit(X_train, y_train)
+y_mean, y_std = gpr.predict(X, return_std=True)
+
+plt.plot(X, y, label="Expected signal")
+plt.scatter(x=X_train[:, 0], y=y_train, color="black", alpha=0.4, label="Observations")
+plt.errorbar(X, y_mean, y_std, label="Posterior mean ± std")
+plt.legend()
+plt.xlabel("X")
+plt.ylabel("y")
+_ = plt.title(
+    (
+        f"Initial: {kernel}\nOptimum: {gpr.kernel_}\nLog-Marginal-Likelihood: "
+        f"{gpr.log_marginal_likelihood(gpr.kernel_.theta)}"
+    ),
+    fontsize=8,
+)
+plt.show()
+
+
+kernel = 1.0 * RBF(length_scale=1e-1, length_scale_bounds=(1e-2, 1e3)) + WhiteKernel(
+    noise_level=1e-2, noise_level_bounds=(1e-10, 1e1)
+)
+gpr = GaussianProcessRegressor(kernel=kernel, alpha=0.0)
+gpr.fit(X_train, y_train)
+y_mean, y_std = gpr.predict(X, return_std=True)
+
+plt.plot(X, y, label="Expected signal")
+plt.scatter(x=X_train[:, 0], y=y_train, color="black", alpha=0.4, label="Observations")
+plt.errorbar(X, y_mean, y_std, label="Posterior mean ± std")
+plt.legend()
+plt.xlabel("X")
+plt.ylabel("y")
+_ = plt.title(
+    (
+        f"Initial: {kernel}\nOptimum: {gpr.kernel_}\nLog-Marginal-Likelihood: "
+        f"{gpr.log_marginal_likelihood(gpr.kernel_.theta)}"
+    ),
+    fontsize=8,
+)
+plt.show()
+
+
+from matplotlib.colors import LogNorm
+length_scale = np.logspace(-2, 4, num=80)
+noise_level = np.logspace(-2, 1, num=80)
+length_scale_grid, noise_level_grid = np.meshgrid(length_scale, noise_level)
+
+log_marginal_likelihood = [
+    gpr.log_marginal_likelihood(theta=np.log([0.36, scale, noise]))
+    for scale, noise in zip(length_scale_grid.ravel(), noise_level_grid.ravel())
+]
+log_marginal_likelihood = np.reshape(log_marginal_likelihood, noise_level_grid.shape)
+
+vmin, vmax = (-log_marginal_likelihood).min(), 50
+level = np.around(np.logspace(np.log10(vmin), np.log10(vmax), num=20), decimals=1)
+plt.contour(
+    length_scale_grid,
+    noise_level_grid,
+    -log_marginal_likelihood,
+    levels=level,
+    norm=LogNorm(vmin=vmin, vmax=vmax),
+)
+plt.colorbar()
+plt.xscale("log")
+plt.yscale("log")
+plt.xlabel("Length-scale")
+plt.ylabel("Noise-level")
+plt.title("Negative log-marginal-likelihood")
+plt.show()
+
+
+kernel = 1.0 * RBF(length_scale=1e1, length_scale_bounds=(1e-2, 1e3)) + WhiteKernel(
+    noise_level=1, noise_level_bounds=(1e-10, 1e1)
+)
+gpr = GaussianProcessRegressor(
+    kernel=kernel, alpha=0.0, n_restarts_optimizer=10, random_state=0
+)
+gpr.fit(X_train, y_train)
+y_mean, y_std = gpr.predict(X, return_std=True)
+
+plt.plot(X, y, label="Expected signal")
+plt.scatter(x=X_train[:, 0], y=y_train, color="black", alpha=0.4, label="Observations")
+plt.errorbar(X, y_mean, y_std, label="Posterior mean ± std")
+plt.legend()
+plt.xlabel("X")
+plt.ylabel("y")
+_ = plt.title(
+    (
+        f"Initial: {kernel}\nOptimum: {gpr.kernel_}\nLog-Marginal-Likelihood: "
+        f"{gpr.log_marginal_likelihood(gpr.kernel_.theta)}"
+    ),
+    fontsize=8,
+)
+plt.show()
