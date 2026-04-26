157e5c2374bc3a615ca50c13bd80538918b3358f
Hespian <kamil.gurlaga1999@gmail.com>
2026-04-26 14:22:48 +0200

    Comparison of kernel ridge and Gaussian process regression

---
+++ Gaussian Processes/comparison_of_kernel_ridge_and_gaussian_process_regression.py
@@ -0,0 +1,49 @@
+# Authors: The scikit-learn developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+## Generate a dataset
+import numpy as np
+rng = np.random.RandomState(0)
+data = np.linspace(0, 30, num=1_000).reshape(-1, 1)
+target = np.sin(data).ravel()
+
+training_sample_indices = rng.choice(np.arange(0, 400), size=40, replace=False)
+training_data = data[training_sample_indices]
+training_noisy_target = target[training_sample_indices] + 0.5 * rng.randn(
+    len(training_sample_indices)
+)
+
+import matplotlib.pyplot as plt
+plt.plot(data, target, label="True signal", linewidth=2)
+plt.scatter(
+    training_data,
+    training_noisy_target,
+    color="black",
+    label="Noisy measurements",
+)
+plt.legend()
+plt.xlabel("data")
+plt.ylabel("target")
+_ = plt.title(
+    "Illustration of the true generative process and \n"
+    "noisy measurements available during training"
+)
+plt.show()
+
+## Limitations of a simple linear model
+from sklearn.linear_model import Ridge
+ridge = Ridge().fit(training_data, training_noisy_target)
+
+plt.plot(data, target, label="True signal", linewidth=2)
+plt.scatter(
+    training_data,
+    training_noisy_target,
+    color="black",
+    label="Noisy measurements",
+)
+plt.plot(data, ridge.predict(data), label="Ridge regression")
+plt.legend()
+plt.xlabel("data")
+plt.ylabel("target")
+_ = plt.title("Limitation of a linear model such as ridge")
+plt.show()
