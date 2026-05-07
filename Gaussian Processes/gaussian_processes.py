e5f30e4f7eb4bef890fbd12c1a4f30c1350968db
Hespian <kamil.gurlaga1999@gmail.com>
2026-05-07 21:33:53 +0200

    GP_kernele

--- Gaussian Processes/gaussian_processes.py
+++ Gaussian Processes/gaussian_processes.py
@@ -1,3 +1,10 @@
 ## 1.7.1. Gaussian Process Regression (GPR)
 ## 1.7.2. Gaussiacn Process Classification (GPC)
 ## 1.7.3. GPC Examples
+## 1.7.4. Kernels for Gaussian Processes
+## 1.7.4.1. Basic kernels
+## 1.7.4.3. Radial basis function (RBF) kernel
+## 1.7.4.4. Metern kernel
+## 1.7.4.5. Rational quadratic kernel
+## 1.7.4.6. Exp-Sine-Squared kernel
+## 1.7.4.7. Dot-Product Kernel
