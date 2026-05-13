edb09dc5e6d0eb3c89282a35487d28b3ab27efdf
Hespian <kamil.gurlaga1999@gmail.com>
2026-05-13 22:52:24 +0200

    illustration of prior and posterior _GPR

--- Gaussian Processes/illustration_of_prior_and_posterior_Gaussian_Process.py
+++ Gaussian Processes/illustration_of_prior_and_posterior_Gaussian_Process.py
@@ -48,3 +48,136 @@ def plot_gpr_samples(gpr_model, n_samples, ax):
     ax.set_ylabel("y")
     ax.set_ylim([-3, 3])
 
+
+## Dataset and Gaussian process generation
+rng = np.random.RandomState(4)
+X_train = rng.uniform(0, 5, 10).reshape(-1, 1)
+y_train = np.sin((X_train[:, 0] - 2.5) ** 2)
+n_samples = 5
+
+### Kernel cookbook
+
+## Radial Basis Function kernel
+from sklearn.gaussian_process import GaussianProcessRegressor
+from sklearn.gaussian_process.kernels import RBF
+
+kernel = 1.0 * RBF(length_scale=1.0, length_scale_bounds=(1e-1, 10.0))
+gpr = GaussianProcessRegressor(kernel=kernel, random_state=0)
+
+fig, axs = plt.subplots(nrows=2, sharex=True, sharey=True, figsize=(10, 8))
+
+# plot prior
+plot_gpr_samples(gpr, n_samples=n_samples, ax=axs[0])
+axs[0].set_title("Samples from prior distribution")
+
+# plot posterior
+gpr.fit(X_train, y_train)
+plot_gpr_samples(gpr, n_samples=n_samples, ax=axs[1])
+axs[1].scatter(X_train[:, 0], y_train, color="red", zorder=10, label="Observations")
+axs[1].legend(bbox_to_anchor=(1.05, 1.5), loc="upper left")
+axs[1].set_title("Samples from posterior distribution")
+
+fig.suptitle("Radial Basis Function kernel", fontsize=18)
+plt.tight_layout()
+plt.show()
+
+print(f"Kernel parameters before fit:\n{kernel})")
+print(
+    f"Kernel parameters after fit: \n{gpr.kernel_} \n"
+    f"Log-likelihood: {gpr.log_marginal_likelihood(gpr.kernel_.theta):.3f}"
+)
+
+## Rational Quadratic kernel
+from sklearn.gaussian_process.kernels import RationalQuadratic
+
+kernel = 1.0 * RationalQuadratic(length_scale=1.0, alpha=0.1, alpha_bounds=(1e-5, 1e15))
+gpr = GaussianProcessRegressor(kernel=kernel, random_state=0)
+
+fig, axs = plt.subplots(nrows=2, sharex=True, sharey=True, figsize=(10, 8))
+
+# plot prior
+plot_gpr_samples(gpr, n_samples=n_samples, ax=axs[0])
+axs[0].set_title("Samples from prior distribution")
+
+# plot posterior
+gpr.fit(X_train, y_train)
+plot_gpr_samples(gpr, n_samples=n_samples, ax=axs[1])
+axs[1].scatter(X_train[:, 0], y_train, color="red", zorder=10, label="Observations")
+axs[1].legend(bbox_to_anchor=(1.05, 1.5), loc="upper left")
+axs[1].set_title("Samples from posterior distribution")
+
+fig.suptitle("Rational Quadratic kernel", fontsize=18)
+plt.tight_layout()
+plt.show()
+
+## Exp-Sine-Squared kernel
+from sklearn.gaussian_process.kernels import ExpSineSquared
+
+kernel = 1.0 * ExpSineSquared(
+    length_scale=1.0,
+    periodicity=3.0,
+    length_scale_bounds=(0.1, 10.0),
+    periodicity_bounds=(1.0, 10.0),
+)
+gpr = GaussianProcessRegressor(kernel=kernel, random_state=0)
+
+fig, axs = plt.subplots(nrows=2, sharex=True, sharey=True, figsize=(10, 8))
+
+# plot prior
+plot_gpr_samples(gpr, n_samples=n_samples, ax=axs[0])
+axs[0].set_title("Samples from prior distribution")
+
+# plot posterior
+gpr.fit(X_train, y_train)
+plot_gpr_samples(gpr, n_samples=n_samples, ax=axs[1])
+axs[1].scatter(X_train[:, 0], y_train, color="red", zorder=10, label="Observations")
+axs[1].legend(bbox_to_anchor=(1.05, 1.5), loc="upper left")
+axs[1].set_title("Samples from posterior distribution")
+
+fig.suptitle("Exp-Sine-Squared kernel", fontsize=18)
+plt.tight_layout()
+plt.show()
+
+## Dot-product kernel
+from sklearn.gaussian_process.kernels import ConstantKernel, DotProduct
+kernel = ConstantKernel(0.1, (0.01, 10.0)) * (DotProduct(sigma_0=1.0, sigma_0_bounds=(0.1, 10.0)) ** 2)
+gpr = GaussianProcessRegressor(kernel=kernel, random_state=0, normalize_y=True)
+
+fig, axs = plt.subplots(nrows=2, sharex=True, sharey=True, figsize=(10, 8))
+
+# plot prior
+plot_gpr_samples(gpr, n_samples=n_samples, ax=axs[0])
+axs[0].set_title("Samples from prior distribution")
+
+# plot posterior
+gpr.fit(X_train, y_train)
+plot_gpr_samples(gpr, n_samples=n_samples, ax=axs[1])
+axs[1].scatter(X_train[:, 0], y_train, color="red", zorder=10, label="Observations")
+axs[1].legend(bbox_to_anchor=(1.05, 1.5), loc="upper left")
+axs[1].set_title("Samples from posterior distribution")
+
+fig.suptitle("Dot-product kernel", fontsize=18)
+plt.tight_layout()
+plt.show()
+
+## Matern kernel
+from sklearn.gaussian_process.kernels import Matern
+kernel = 1.0 * Matern(length_scale=1.0, length_scale_bounds=(1e-1, 10.0), nu=1.5)
+gpr = GaussianProcessRegressor(kernel=kernel, random_state=0)
+
+fig, axs = plt.subplots(nrows=2, sharex=True, sharey=True, figsize=(10, 8))
+
+# plot prior
+plot_gpr_samples(gpr, n_samples=n_samples, ax=axs[0])
+axs[0].set_title("Samples from prior distribution")
+
+# plot posterior
+gpr.fit(X_train, y_train)
+plot_gpr_samples(gpr, n_samples=n_samples, ax=axs[1])
+axs[1].scatter(X_train[:, 0], y_train, color="red", zorder=10, label="Observations")
+axs[1].legend(bbox_to_anchor=(1.05, 1.5), loc="upper left")
+axs[1].set_title("Samples from posterior distribution")
+
+fig.suptitle("Matern kernel", fontsize=18)
+plt.tight_layout()
+plt.show()
