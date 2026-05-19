b63df4922322a71932d831356ce5e2d73b882d32
Hespian <kamil.gurlaga1999@gmail.com>
2026-05-19 21:37:01 +0200

    Principal Component Regression vs Partial Least Squares Regression

---
+++ Cross decomposition/principal_component_regression_vs_partial_least_squares_regression.py
@@ -0,0 +1,59 @@
+# Authors: The scikit-learn developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+## The data
+import matplotlib.pyplot as plt
+import numpy as np
+
+from sklearn.decomposition import PCA
+
+rng = np.random.RandomState(0)
+n_samples = 500
+cov = [[3, 3], [3, 4]]
+X = rng.multivariate_normal(mean=[0, 0], cov=cov, size=n_samples)
+pca = PCA(n_components=2).fit(X)
+
+
+plt.scatter(X[:, 0], X[:, 1], alpha=0.3, label="samples")
+for i, (comp, var) in enumerate(zip(pca.components_, pca.explained_variance_)):
+    comp = comp * var  # scale component by its variance explanation power
+    plt.plot(
+        [0, comp[0]],
+        [0, comp[1]],
+        label=f"Component {i}",
+        linewidth=5,
+        color=f"C{i + 2}",
+    )
+plt.gca().set(
+    aspect="equal",
+    title="2-dimensional dataset with principal components",
+    xlabel="first feature",
+    ylabel="second feature",
+)
+plt.legend()
+plt.show()
+
+
+y = X.dot(pca.components_[1]) + rng.normal(size=n_samples) / 2
+
+fig, axes = plt.subplots(1, 2, figsize=(10, 3))
+axes[0].scatter(X.dot(pca.components_[0]), y, alpha=0.3)
+axes[0].set(xlabel="Projected data onto first PCA component", ylabel="y")
+axes[1].scatter(X.dot(pca.components_[1]), y, alpha=0.3)
+axes[1].set(xlabel="Projected data onto second PCA component", ylabel="y")
+plt.tight_layout()
+plt.show()
+
+## Projection on one component and predictive power
+from sklearn.cross_decomposition import PLSRegression
+from sklearn.decomposition import PCA
+from sklearn.linear_model import LinearRegression
+from sklearn.model_selection import train_test_split
+from sklearn.pipeline import make_pipeline
+from sklearn.preprocessing import StandardScaler
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, random_state=rng)
+
+pcr = make_pipeline(StandardScaler(), PCA(n_components=1), LinearRegression)
+pcr.fit(X_train, y_train)
+pca = pcr.normal_steps["pca"] # retrive the 
