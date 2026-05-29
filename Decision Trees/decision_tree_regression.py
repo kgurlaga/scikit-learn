24fcd92aa5aea042996fbaffc580b99d3b53bd58
Hespian <kamil.gurlaga1999@gmail.com>
2026-05-29 19:21:39 +0200

    Decision Tree Regression

---
+++ Decision Trees/decision_tree_regression.py
@@ -0,0 +1,90 @@
+# Authors: The scikit-learn developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+## Create a random 1D dataset
+import numpy as np
+rng = np.random.RandomState(1)
+X = np.sort(5 * rng.rand(80, 1), axis=0)
+y = np.sin(X).ravel()
+y[::5] += 3 * (0.5 - rng.rand(16))
+
+## Fit regression model
+from sklearn.tree import DecisionTreeRegressor
+
+regr_1 = DecisionTreeRegressor(max_depth=2)
+regr_2 = DecisionTreeRegressor(max_depth=5)
+regr_1.fit(X, y)
+regr_2.fit(X, y)
+
+## Predict
+X_test = np.arange(0.0, 5.0, 0.01)[:, np.newaxis]
+y_1 = regr_1.predict(X_test)
+y_2 = regr_2.predict(X_test)
+
+## Plot the results
+import matplotlib.pyplot as plt
+plt.figure()
+plt.scatter(X, y, s=20, edgecolor="black", c="darkorange", label="data")
+plt.plot(X_test, y_1, color="cornflowerblue", label="max_depth=2", linewidth=2)
+plt.plot(X_test, y_2, color="yellowgreen", label="max_depth=5", linewidth=2)
+plt.xlabel("data")
+plt.ylabel("target")
+plt.title("Decision Tree Regression")
+plt.legend()
+plt.show()
+
+## Decision Tree Regression with Multi-Output Targets
+rng = np.random.RandomState(1)
+X = np.sort(200 * rng.rand(100, 1) - 100, axis=0)
+y = np.array([np.pi * np.sin(X).ravel(), np.pi * np.cos(X).ravel()]).T
+y[::5, :] += 0.5 - rng.rand(20, 2)
+
+## Fit regression model
+regr_1 = DecisionTreeRegressor(max_depth=2)
+regr_2 = DecisionTreeRegressor(max_depth=5)
+regr_3 = DecisionTreeRegressor(max_depth=8)
+regr_1.fit(X, y)
+regr_2.fit(X, y)
+regr_3.fit(X, y)
+
+## Predict
+X_test = np.arange(-100.0, 100.0, 0.01)[:, np.newaxis]
+y_1 = regr_1.predict(X_test)
+y_2 = regr_2.predict(X_test)
+y_3 = regr_3.predict(X_test)
+
+## Plot the results
+plt.figure()
+s = 25
+plt.scatter(y[:, 0], y[:, 1], c="yellow", s=s, edgecolor="black", label="data")
+plt.scatter(
+    y_1[:, 0],
+    y_1[:, 1],
+    c="cornflowerblue",
+    s=s,
+    edgecolor="black",
+    label="max_depth=2",
+)
+plt.scatter(
+    y_2[:, 0],
+    y_2[:, 1],
+    c="red",
+    s=s,
+    edgecolor="black",
+    label="max_depth=5",
+)
+plt.scatter(
+    y_3[:, 0],
+    y_3[:, 1],
+    c="blue",
+    s=s,
+    edgecolor="black",
+    label="max_depth=8",
+)
+plt.xlim([-6, 6])
+plt.ylim([-6, 6])
+plt.xlabel("target 1")
+plt.ylabel("target 2")
+plt.title("Multi-output Decision Tree Regression")
+plt.legend(loc="best")
+plt.show()
--- Decision Trees/decision_trees.py
+++ Decision Trees/decision_trees.py
@@ -16,3 +16,11 @@ clf = tree.DecisionTreeClassifier()
 clf = clf.fit(X, y)
 tree.plot_tree(clf)
 plt.show()
+
+## 1.10.2. Regression
+from sklearn import tree
+X = [[0, 0], [2, 2]]
+y = [0.5, 2.5]
+clf = tree.DecisionTreeRegressor()
+clf = clf.fit(X, y)
+clf.predict([[1, 1]])
