5a599256526ddfe0ae8209625fc7f39838c7e447
Hespian <kamil.gurlaga1999@gmail.com>
2026-06-10 22:37:11 +0200

    Missing Values Support & Minimal Cost-Complexity Pruning

--- Decision Trees/decision_trees.py
+++ Decision Trees/decision_trees.py
@@ -26,3 +26,34 @@ clf = clf.fit(X, y)
 clf.predict([[1, 1]])
 
 ## 1.10.3. Multi-output problems
+
+## 1.10.4. Complexity
+
+## 1.10.5. Tips on practical use
+
+## 1.10.6. Tree algorithms: ID3, C4.5, C5.0 and CART
+
+## 1.10.7. Mathematical formulation
+
+## 1.10.8. Missing Values Support
+from sklearn.tree import DecisionTreeClassifier
+import numpy as np
+
+X = np.array([0, 1, 6, np.nan]).reshape(-1, 1)
+y = [0, 0, 1, 1]
+tree = DecisionTreeClassifier(random_state=0).fit(X, y)
+tree.predict(X)
+
+X = np.array([np.nan, -1, np.nan, 1]).reshape(-1, 1)
+y = [0, 0, 1, 1]
+tree = DecisionTreeClassifier(random_state=0, max_depth=1).fit(X, y)
+X_test = np.array([np.nan]).reshape(-1, 1)
+tree.predict(X_test)
+
+X = np.array([0, 1, 2, 3]).reshape(-1, 1)
+y = [0, 1, 1, 1]
+tree = DecisionTreeClassifier(random_state=0).fit(X, y)
+X_test = np.array([np.nan]).reshape(-1, 1)
+tree.predict(X_test)
+
+## 1.10.9. Minimal Cost-Complexity Pruning
