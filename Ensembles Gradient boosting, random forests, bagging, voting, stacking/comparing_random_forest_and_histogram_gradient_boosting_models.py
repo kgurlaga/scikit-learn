fea23a06abc43fcfc55072ae4a531f04e4f9a5e5
Hespian <kamil.gurlaga1999@gmail.com>
2026-06-27 22:15:56 +0200

    Comparing Random Forests and Histogram Gradient Boosting models

---
+++ Ensembles Gradient boosting, random forests, bagging, voting, stacking/comparing_random_forest_and_histogram_gradient_boosting_models.py
@@ -0,0 +1,119 @@
+# Authors: The scikit-learn developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+## Load dataset
+from sklearn. datasets import fetch_california_housing
+
+X, y = fetch_california_housing(return_X_y=True, as_frame=True)
+n_samples, n_features = X.shape
+print(f"The dataset consist of {n_samples} sample and {n_features} features")
+
+## Compute score and computation times
+import joblib
+N_CORES = joblib.cpu_count(only_physical_cores=True)
+print(f"Number of physical cores: {N_CORES}")
+
+
+import pandas as pd
+from sklearn.ensemble import HistGradientBoostingRegressor, RandomForestRegressor
+from sklearn.model_selection import GridSearchCV, KFold
+
+models = {
+    "Random Forest": RandomForestRegressor(
+        min_samples_leaf=5, random_state=0, n_jobs=N_CORES
+    ),
+    "Hist Gradient Boosting": HistGradientBoostingRegressor(
+        max_leaf_nodes=15, random_state=0, early_stopping=False
+    ),
+}
+param_grids = {
+    "Random Forest": {"n_estimators": [10, 20, 50, 100]},
+    "Hist Gradient Boosting": {"max_iter": [10, 20, 50, 100, 300, 500]},
+}
+cv = KFold(n_splits=4, shuffle=True, random_state=0)
+results = []
+for name, model in models.items():
+    grid_search = GridSearchCV(
+        estimator=model,
+        param_grid=param_grids[name],
+        return_train_score=True,
+        cv=cv,
+    ).fit(X, y)
+    result = {"model": name, "cv_results": pd.DataFrame(grid_search.cv_results_)}
+    results.append(result)
+
+# Plot results
+import plotly.colors as colors
+import plotly.express as px
+from plotly.subplots import make_subplots
+import matplotlib.pyplot as plt
+
+fig = make_subplots(
+    rows=1,
+    cols=2,
+    shared_yaxes=True,
+    subplot_titles=["Train time vs score", "Predict time vs score"],
+)
+model_names = [result["model"] for result in results]
+colors_list = colors.qualitative.Plotly * (
+    len(model_names) // len(colors.qualitative.Plotly) + 1
+)
+
+for idx, result in enumerate(results):
+    cv_results = result["cv_results"].round(3)
+    model_name = result["model"]
+    param_name = next(iter(param_grids[model_name].keys()))
+    cv_results[param_name] = cv_results["param_" + param_name]
+    cv_results["model"] = model_name
+
+    scatter_fig = px.scatter(
+        cv_results,
+        x="mean_fit_time",
+        y="mean_test_score",
+        error_x="std_fit_time",
+        error_y="std_test_score",
+        hover_data=param_name,
+        color="model",
+    )
+    line_fig = px.line(
+        cv_results,
+        x="mean_fit_time",
+        y="mean_test_score",
+    )
+
+    scatter_trace = scatter_fig["data"][0]
+    line_trace = line_fig["data"][0]
+    scatter_trace.update(marker=dict(color=colors_list[idx]))
+    line_trace.update(line=dict(color=colors_list[idx]))
+    fig.add_trace(scatter_trace, row=1, col=1)
+    fig.add_trace(line_trace, row=1, col=1)
+
+    scatter_fig = px.scatter(
+        cv_results,
+        x="mean_score_time",
+        y="mean_test_score",
+        error_x="std_score_time",
+        error_y="std_test_score",
+        hover_data=param_name,
+    )
+    line_fig = px.line(
+        cv_results,
+        x="mean_score_time",
+        y="mean_test_score",
+    )
+
+    scatter_trace = scatter_fig["data"][0]
+    line_trace = line_fig["data"][0]
+    scatter_trace.update(marker=dict(color=colors_list[idx]))
+    line_trace.update(line=dict(color=colors_list[idx]))
+    fig.add_trace(scatter_trace, row=1, col=2)
+    fig.add_trace(line_trace, row=1, col=2)
+
+fig.update_layout(
+    xaxis=dict(title="Train time (s) - lower is better"),
+    yaxis=dict(title="Test R2 score - higher is better"),
+    xaxis2=dict(title="Predict time (s) - lower is better"),
+    legend=dict(x=0.72, y=0.05, traceorder="normal", borderwidth=1),
+    title=dict(x=0.5, text="Speed-score trade-off of tree-based ensembles"),
+)
+fig.show()
