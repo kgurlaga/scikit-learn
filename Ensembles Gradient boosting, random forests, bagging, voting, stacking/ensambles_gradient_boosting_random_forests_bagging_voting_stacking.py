50ed02bbff8485bfe85b13b4d6a44af4681aa005
Hespian <kamil.gurlaga1999@gmail.com>
2026-06-30 23:05:48 +0200

    Missing values support

--- Ensembles Gradient boosting, random forests, bagging, voting, stacking/ensambles_gradient_boosting_random_forests_bagging_voting_stacking.py
+++ Ensembles Gradient boosting, random forests, bagging, voting, stacking/ensambles_gradient_boosting_random_forests_bagging_voting_stacking.py
@@ -1,2 +1,30 @@
 ## 1.11.1. Gradient-boosted trees
 ## 1.11.1.1. Histogram-based gradient boosting
+## 1.11.1.1.1 Usage
+from sklearn.ensemble import HistGradientBoostingClassifier
+from sklearn.datasets import make_hastie_10_2
+
+X, y = make_hastie_10_2(random_state=0)
+X_train, X_test = X[:2000], X[2000:]
+y_train, y_test = y[:2000], y[2000:]
+
+clf = HistGradientBoostingClassifier(max_iter=100).fit(X_train, y_train)
+clf.score(X_test, y_test)
+
+## 1.11.1.1.2 Missing values support
+from sklearn.ensemble import HistGradientBoostingClassifier
+import numpy as np
+
+X = np.array([0, 1, 2, np.nan]).reshape(-1, 1)
+y = [0, 0, 1, 1]
+
+gbdt = HistGradientBoostingClassifier(min_samples_leaf=1).fit(X, y)
+gbdt.predict(X)
+
+X = np.array([0, np.nan, 1, 2, np.nan]).reshape(-1, 1)
+y = [0, 1, 0, 0, 1]
+gbdt = HistGradientBoostingClassifier(min_samples_leaf=1,
+                                      max_depth=2,
+                                      learning_rate=1,
+                                      max_iter=1).fit(X, y)
+gbdt.predict(X)
