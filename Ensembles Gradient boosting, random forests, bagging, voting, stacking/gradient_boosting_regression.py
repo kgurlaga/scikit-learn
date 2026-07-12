014805d451749fbb77a2137f8d418600c3603461
Hespian <kamil.gurlaga1999@gmail.com>
2026-07-12 16:08:21 +0200

    Gradient Boosting regression

---
+++ Ensembles Gradient boosting, random forests, bagging, voting, stacking/gradient_boosting_regression.py
@@ -0,0 +1,88 @@
+# Authors: The scikit-learn developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+import matplotlib
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn import datasets, ensemble
+from sklearn.inspection import permutation_importance
+from sklearn.metrics import mean_squared_error
+from sklearn.model_selection import train_test_split
+from sklearn.utils.fixes import parse_version
+
+## Load data
+diabetes = datasets.load_diabetes()
+X, y = diabetes.data, diabetes.target
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.1, random_state=13
+)
+params = {
+    "n_estimators": 500,
+    "max_depth": 4,
+    "min_samples_split": 5,
+    "learning_rate": 0.01,
+    "loss": "squared_error",
+}
+
+## Fit regression model
+reg = ensemble.GradientBoostingRegressor(**params)
+reg.fit(X_train, y_train)
+
+mse = mean_squared_error(y_test, reg.predict(X_test))
+print("The mean squared error (MSE) on test set: {:.4f}".format(mse))
+
+## Plot training deviance
+test_score = np.zeros((params["n_estimators"],), dtype=np.float64)
+for i, y_pred in enumerate(reg.staged_predict(X_test)):
+    test_score[i] = mean_squared_error(y_test, y_pred)
+
+fig = plt.figure(figsize=(6, 6))
+plt.subplot(1, 1, 1)
+plt.title("Deviance")
+plt.plot(
+    np.arange(params["n_estimators"]) + 1,
+    reg.train_score_,
+    "b-",
+    label="Training Set Deviance",
+)
+plt.plot(
+    np.arange(params["n_estimators"]) + 1, test_score, "r-", label="Test Set Deviance"
+)
+plt.legend(loc="upper right")
+plt.xlabel("Boosting Iterations")
+plt.ylabel("Deviance")
+fig.tight_layout()
+plt.show()
+
+## Plot feature importancefeature_importance = reg.feature_importances_
+sorted_idx = np.argsort(feature_importance)
+pos = np.arange(sorted_idx.shape[0]) + 0.5
+fig = plt.figure(figsize=(12, 6))
+plt.subplot(1, 2, 1)
+plt.barh(pos, feature_importance[sorted_idx], align="center")
+plt.yticks(pos, np.array(diabetes.feature_names)[sorted_idx])
+plt.title("Feature Importance (MDI)")
+
+result = permutation_importance(
+    reg, X_test, y_test, n_repeats=10, random_state=42, n_jobs=2
+)
+sorted_idx = result.importances_mean.argsort()
+plt.subplot(1, 2, 2)
+
+# `labels` argument in boxplot is deprecated in matplotlib 3.9 and has been
+# renamed to `tick_labels`. The following code handles this, but as a
+# scikit-learn user you probably can write simpler code by using `labels=...`
+# (matplotlib < 3.9) or `tick_labels=...` (matplotlib >= 3.9).
+tick_labels_parameter_name = (
+    "tick_labels"
+    if parse_version(matplotlib.__version__) >= parse_version("3.9")
+    else "labels"
+)
+tick_labels_dict = {
+    tick_labels_parameter_name: np.array(diabetes.feature_names)[sorted_idx]
+}
+plt.boxplot(result.importances[sorted_idx].T, vert=False, **tick_labels_dict)
+plt.title("Permutation Importance (test set)")
+fig.tight_layout()
+plt.show()
