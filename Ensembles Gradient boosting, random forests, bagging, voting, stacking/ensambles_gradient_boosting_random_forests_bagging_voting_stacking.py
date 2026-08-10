6e378c4c6b43aa59cd054da3880af706ae6544df
Hespian <kamil.gurlaga1999@gmail.com>
2026-08-10 22:28:22 +0200

    Voting Classifier

--- Ensembles Gradient boosting, random forests, bagging, voting, stacking/ensambles_gradient_boosting_random_forests_bagging_voting_stacking.py
+++ Ensembles Gradient boosting, random forests, bagging, voting, stacking/ensambles_gradient_boosting_random_forests_bagging_voting_stacking.py
@@ -159,3 +159,33 @@ from sklearn.ensemble import BaggingClassifier
 from sklearn.neighbors import KNeighborsClassifier
 bagging = BaggingClassifier(KNeighborsClassifier(), max_samples=0.5, max_features=0.5)
 print(bagging)
+
+## 1.11.4. Voting Classifier
+
+## 1.11.4.1. Majority Class Labels (Majority/Hard Voting)
+
+## 1.11.4.2. Usage
+from sklearn import datasets
+from sklearn.model_selection import cross_val_score
+from sklearn.linear_model import LogisticRegression
+from sklearn.naive_bayes import GaussianNB
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.ensemble import VotingClassifier
+
+iris = datasets.load_iris()
+X, y = iris.data[:, 1:3], iris.target
+
+clf1 = LogisticRegression(random_state=1)
+clf2 = RandomForestClassifier(n_estimators=50, random_state=1)
+clf3 = GaussianNB()
+
+eclf = VotingClassifier(
+    estimators=[('lr', clf1), ('rf', clf2), ('gnb', clf3)],
+    voting='hard'
+)
+
+for clf, label in zip([clf1, clf2, clf3, eclf], ['Logistic Regression', 'Random Forest', 'naive Bayes', 'Ensemble']):
+    scores = cross_val_score(clf, X, y, scoring='accuracy', cv=5)
+    print("Accuracy: %0.2f (+/- %0.2f) [%s]" % (scores.mean(), scores.std(), label))
+
+## 1.11.4.3. Weighted Average Probabilities (Soft Voting)
