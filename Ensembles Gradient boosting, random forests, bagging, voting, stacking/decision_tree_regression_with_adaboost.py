aa98cd58ac3e8b2a126d08877364946ae55cbe93
Hespian <kamil.gurlaga1999@gmail.com>
2026-08-27 21:38:50 +0200

    Decision Tree Regression with AdaBoost

---
+++ Ensembles Gradient boosting, random forests, bagging, voting, stacking/decision_tree_regression_with_adaboost.py
@@ -0,0 +1,39 @@
+# Preparing the data
+# Authors: The scikit-learn developers
+# SPDX-License-Identifier: BSD-3-Clause
+
+import numpy as np
+rng = np.random.RandomState(1)
+X = np.linspace(0, 6, 100)[:, np.newaxis]
+y = np.sin(X).ravel() + np.sin(6 * X).ravel() + rng.normal(0, 0.1, X.shape[0])
+
+# Training and prediction with DecisionTree and AdaBoost Regressors
+from sklearn.ensemble import AdaBoostRegressor
+from sklearn.tree import DecisionTreeRegressor
+
+regr_1 = DecisionTreeRegressor(max_depth=4)
+regr_2 = AdaBoostRegressor(
+    DecisionTreeRegressor(max_depth=4), n_estimators=300, random_state=rng
+)
+
+regr_1.fit(X, y)
+regr_2.fit(X, y)
+
+y_1 = regr_1.predict(X)
+y_2 = regr_2.predict(X)
+
+## Plotting the results
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+colors = sns.color_palette("colorblind")
+
+plt.figure()
+plt.scatter(X, y, color=colors[0], label="training samples")
+plt.plot(X, y_1, color=colors[1], label="n_estimators=1", linewidth=2)
+plt.plot(X, y_2, color=colors[2], label="n_estimators=300", linewidth=2)
+plt.xlabel("data")
+plt.ylabel("target")
+plt.title("Boosted Decision Tree Regression")
+plt.legend()
+plt.show()
