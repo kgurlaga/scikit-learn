703bfc7fb1475bc7b7fd529ff1207474902332a5
Hespian <kamil.gurlaga1999@gmail.com>
2026-09-10 12:07:45 +0200

    1.12.3. Multiclass-multioutput classification

--- Multiclass and multioutput algorithms/mutliclass_and_multioutput_algorithms.py
+++ Multiclass and multioutput algorithms/mutliclass_and_multioutput_algorithms.py
@@ -49,3 +49,24 @@ print(y_sparse)
 ## 1.12.2.2. MultiOutputClassifier
 
 ## 1.12.2.3. ClassifierChain
+
+## 1.12.3. Multiclass-multioutput classification
+from sklearn.datasets import make_classification
+from sklearn.multioutput import MultiOutputClassifier
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.utils import shuffle
+import numpy as np
+X, y1 = make_classification(n_samples=10, n_features=100, n_informative=30, n_classes=3, random_state=1)
+y2 = shuffle(y1, random_state=1)
+y3 = shuffle(y1, random_state=2)
+Y = np.vstack((y1, y2, y3)).T
+n_samples, n_features = X.shape
+n_outputs = Y.shape[1]
+n_classes = 3
+forest = RandomForestClassifier(random_state=1)
+multi_target_forest = MultiOutputClassifier(forest, n_jobs=2)
+multi_target_forest.fit(X, Y).predict(X)
+
+## 1.12.3.1. Target format
+y = np.array([['apple', 'green'], ['orange', 'orange'], ['pear', 'green']])
+print(y)
